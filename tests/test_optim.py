acd15fc2abd59b87d019907039b69853d3c95a03
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-08 21:05:10 -0500

    Use nll instead of twice_nll

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -343,11 +343,11 @@ def test_optim(backend, source, spec, mu):
 
     optim = pyhf.optimizer
 
-    result = optim.minimize(pyhf.infer.mle.twice_nll, data, pdf, init_pars, par_bounds)
+    result = optim.minimize(pyhf.infer.mle.nll, data, pdf, init_pars, par_bounds)
     assert pyhf.tensorlib.tolist(result)
 
     result = optim.minimize(
-        pyhf.infer.mle.twice_nll,
+        pyhf.infer.mle.nll,
         data,
         pdf,
         init_pars,
@@ -367,11 +367,11 @@ def test_optim_with_value(backend, source, spec, mu):
 
     optim = pyhf.optimizer
 
-    result = optim.minimize(pyhf.infer.mle.twice_nll, data, pdf, init_pars, par_bounds)
+    result = optim.minimize(pyhf.infer.mle.nll, data, pdf, init_pars, par_bounds)
     assert pyhf.tensorlib.tolist(result)
 
     result, fitted_val = optim.minimize(
-        pyhf.infer.mle.twice_nll,
+        pyhf.infer.mle.nll,
         data,
         pdf,
         init_pars,
@@ -395,11 +395,11 @@ def test_optim_uncerts(backend, source, spec, mu):
 
     optim = pyhf.optimizer
 
-    result = optim.minimize(pyhf.infer.mle.twice_nll, data, pdf, init_pars, par_bounds)
+    result = optim.minimize(pyhf.infer.mle.nll, data, pdf, init_pars, par_bounds)
     assert pyhf.tensorlib.tolist(result)
 
     result = optim.minimize(
-        pyhf.infer.mle.twice_nll,
+        pyhf.infer.mle.nll,
         data,
         pdf,
         init_pars,
